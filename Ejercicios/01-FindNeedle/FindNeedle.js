e0899939e495eebe139fbea4165d4ce7a86dadda
David Orlando Miranda Roa <domirandar@unal.edu.co>
2024-01-01 18:11:54 -0500

    Haystack-needle

--- Ejercicios/01-FindNeedle/FindNeedle.js
+++ Ejercicios/01-FindNeedle/FindNeedle.js
@@ -1,5 +1,18 @@
 function FindNeedle (haystack, needle) {
   // Your code here:
+  let count = 0;
+  for (const i of haystack) {
+    for (const j of needle) {
+      if (i === j) {
+        count ++
+        console.log(i);
+        console.log(j);
+        
+      }
+    }
+    
+    
+  }
 
 }
 
