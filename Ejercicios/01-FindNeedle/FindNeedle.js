ca51c8508b9103bccf770cf67a7811c9e82e8a8f
David Orlando Miranda Roa <domirandar@unal.edu.co>
2024-01-11 17:32:19 -0500

    Test running, 1st exercice 2/3

--- Ejercicios/01-FindNeedle/FindNeedle.js
+++ Ejercicios/01-FindNeedle/FindNeedle.js
@@ -1,5 +1,22 @@
 function FindNeedle (haystack, needle) {
-  // Your code here:
+  let index = 0;
+  for (let i = 0; i < haystack.length; i++) {
+    for (let j = 0; j < needle.length; j++) {
+      if (haystack[i]===needle[j]) {
+        if (haystack[i+1]===needle[j+1]) {
+          
+        index ++;
+        } else {
+          index = -1;
+          break;
+        }
+      } else {
+        continue;
+      }
+    };
+    
+  }
+  return index;
 
 }
 
