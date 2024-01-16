c364d345c013de669b976625633835e1f6b460e8
David Orlando Miranda Roa <domirandar@unal.edu.co>
2024-01-16 16:51:33 -0500

    SumArray without extras finished

--- Ejercicios/02-SumArray/SumArray.js
+++ Ejercicios/02-SumArray/SumArray.js
@@ -1,6 +1,16 @@
 function SumArray (arr, n) {
-  // Your code here:
 
+  for (const i of arr) {
+    for (let j = 1; j < arr.length; j++) {
+      
+    if (i+arr[j]===n) {
+      return true;
+    }
+      
+    }
+    
+  }
+return false;
 }
 
 module.exports = SumArray
